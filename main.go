4528d8f5f343937c6ce6482173fb390de296c80f
Alex Nichol <unixpickle@gmail.com>
2017-04-25 18:03:34 -0400

    use facebook messenger for contact form

--- main.go
+++ main.go
@@ -4,27 +4,28 @@ import (
 	"flag"
 	"log"
 	"net/http"
-	"net/smtp"
 	"os"
 	"strconv"
 
 	"github.com/unixpickle/essentials"
+	"github.com/unixpickle/fbmsgr"
 )
 
-var EmailUsername, EmailPassword string
-var EmailRecipient string
+var FacebookEmail, FacebookPassword string
+var FacebookRecipient string
 
 func main() {
 	var port int
 	flag.IntVar(&port, "port", 1337, "port number")
 	flag.Parse()
 
-	EmailUsername = os.Getenv("CONTACT_EMAIL")
-	EmailPassword = os.Getenv("CONTACT_PASSWORD")
-	EmailRecipient = os.Getenv("CONTACT_RECIPIENT")
-	if EmailUsername == "" || EmailPassword == "" || EmailRecipient == "" {
-		essentials.Die("Set CONTACT_EMAIL, CONTACT_PASSWORD, and " +
-			"CONTACT_RECIPIENT env vars.")
+	FacebookEmail = os.Getenv("CONTACT_EMAIL")
+	FacebookPassword = os.Getenv("CONTACT_PASSWORD")
+	FacebookRecipient = os.Getenv("CONTACT_RECIPIENT")
+	if FacebookEmail == "" || FacebookPassword == "" ||
+		FacebookRecipient == "" {
+		essentials.Die("Set CONTACT_EMAIL, CONTACT_PASSWORD, CONTACT_RECIPIENT " +
+			"env vars")
 	}
 	http.HandleFunc("/contact", ContactForm)
 	http.Handle("/", http.FileServer(http.Dir(".")))
@@ -35,20 +36,18 @@ func main() {
 }
 
 func ContactForm(w http.ResponseWriter, r *http.Request) {
-	auth := smtp.PlainAuth("", EmailUsername, EmailPassword, "smtp.gmail.com")
-	bodyStr := "Email sent from contact form.\n\n" +
+	client, err := fbmsgr.Auth(FacebookEmail, FacebookPassword)
+	if err != nil {
+		log.Println("Login error:", err)
+		return
+	}
+	defer client.Close()
+	bodyStr := "Message sent from contact form.\n\n" +
 		"Subject: " + r.FormValue("subject") + "\n" +
 		"Email: " + r.FormValue("email") + "\n" +
 		"Name: " + r.FormValue("message") + "\n\n" +
 		r.FormValue("body")
-	err := smtp.SendMail(
-		"smtp.gmail.com:587",
-		auth,
-		EmailUsername,
-		[]string{EmailRecipient},
-		[]byte(bodyStr),
-	)
-	if err != nil {
-		log.Print(err)
+	if err := client.SendText(FacebookRecipient, bodyStr); err != nil {
+		log.Println("Send error:", err)
 	}
 }
